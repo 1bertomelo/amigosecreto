74b45b94f6f6aa12ab331fb2cc9ad37b0175c6d3
1bertomelo <1bertomelo@gmail.com>
2020-05-10 20:55:59 -0300

    ajustes api participante com BD mongo

--- backend/amigosecreto-microservice/participante-service/src/index.js
+++ backend/amigosecreto-microservice/participante-service/src/index.js
@@ -1,17 +1,17 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const routes = require('./routes/routes');
 
 const app = express();
 
 app.use(express.json());
 
-app.get('/:nome', (request, response) => {
-    return response.send("Olá Mundo " + `${request.params.nome}`);
+app.use(routes);
+
+mongoose.connect('mongodb+srv://humberto:humberto@cluster0-fbd4q.azure.mongodb.net/test?retryWrites=true&w=majority',{
+    useNewUrlParser: true,
+    useUnifiedTopology: true
 });
 
-app.post('/participante',(request , response) =>{
-    console.log(request.body);
-    const { nome } = request.body;
-    return response.json({"mensagem":`Olá Mundo ${nome}`});
-});
 
 app.listen(3333);
