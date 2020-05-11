08a32f05a514ccecf3e1311f7e51ec6e60c3faae
1bertomelo <1bertomelo@gmail.com>
2020-05-11 19:57:57 -0300

    versão 1 api grupo

---
+++ backend/amigosecreto-microservice/grupo-service/src/repositories/grupoRepository.js
@@ -0,0 +1,110 @@
+const Grupo = require('../models/grupo');
+
+module.exports = {
+    async create (grupo){
+        try{
+            let { nome, valorMinimo , valorMaximo, dataSorteio , responsavel }  = grupo;
+            const retornGrupo = await Grupo.create({
+                nome,
+                valorMinimo,
+                valorMaximo ,
+                dataSorteio,
+                responsavel
+         }); 
+         return retornGrupo;
+        }
+        catch (err) {
+            console.log("Erro " + err);
+        }
+        return null;
+    },
+    async edit (Grupo){
+        try{
+            let { _id, nome, valorMinimo , valorMaximo, dataSorteio }  = Grupo;
+            const retornGrupo = await Grupo.updateOne(
+                { _id }, {$set : { nome , valorMinimo ,valorMaximo, dataSorteio }}
+         ); 
+         return retornGrupo;
+        }
+        catch (err) {
+            console.log("Erro " + err);
+        }
+        return null;
+    },
+    async getById (_id){
+        try{
+            
+            const retornGrupo = await Grupo.find({ _id }); 
+            return retornGrupo;
+        }
+        catch (err) {
+            console.log("Erro " + err);
+        }
+        return null;
+    },
+    async delete (_id){
+        try{
+            
+            const retornGrupo = await Grupo.deleteOne({ _id : _id}); 
+            return retornGrupo;
+        }
+        catch (err) {
+            console.log("Erro " + err);
+        }
+        return null;
+    },
+    async addParticipante (grupo){
+        try{           
+            let { _id , participantes } = grupo; 
+            const retornGrupo = await Grupo.updateOne({ _id }, { $push : { participantes }});
+             return retornGrupo;
+        }
+        catch (err) {
+            console.log("Erro " + err);
+        }
+        return null;
+    },
+    async getGrupoByIdResponsavel (_idResponsavel){
+        try{
+            console.log("oi");
+            console.log(_idResponsavel);
+            const retornGrupo = await Grupo.find({"responsavel._idResponsavel" : _idResponsavel}); 
+            console.log(retornGrupo);
+            
+            
+            return retornGrupo;
+        }
+        catch (err) {
+            console.log("Erro " + err);
+        }
+        return null;
+    },
+    async getGrupoByIdParticipante (_idParticipante){
+        try{
+            console.log("oi");
+            const retornGrupo = await Grupo.find({	participantes : {$elemMatch :{ "_idParticipante" : _idParticipante}}}); 
+            console.log(retornGrupo);
+            
+
+            return retornGrupo;
+        }
+        catch (err) {
+            console.log("Erro " + err);
+        }
+        return null;
+    },
+    async delParticipante (_id, _idParticipante){
+        try{
+            
+            const retornGrupo = await Grupo.updateOne({ _id }, { $pull : { participantes : { _idParticipante }}});
+            return retornGrupo;
+        }
+        catch (err) {
+            console.log("Erro " + err);
+        }
+        return null;
+    },
+    
+
+
+}
